f0d00713e8f16bcda4473d92c7a175bd502d2fe0
Manu080405 <manurejimon12@gmail.com>
2025-11-06 19:22:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection With MongoDB
-mongoose.connect("mongodb+srv://manu0804:manu@cluster0.uyaxilh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("");
 
 // paste your mongoDB Connection string above with password
 // password should not contain '@' special character
